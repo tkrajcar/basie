23fe829822047c79ca5eb33e8f7a8121b876ac7e
allegroconmolto <allegroconmolto@0bc681b0-650d-0410-bb77-fdbd1d7d6fd8>
2005-10-18 01:05:49 +0000

    Initial add of website

---
+++ trunk/website/include/bottom.php
@@ -0,0 +1,57 @@
+		</td>
+	</tr>
+	<tr>
+		<td colspan="4">
+			<img id="layout_24" src="images/layout_24.gif" width="268" height="22" alt="" /></td>
+		<td colspan="7">
+			<img id="layout_25" src="images/layout_25.gif" width="479" height="22" alt="" /></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/spacer.gif" width="128" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="82" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="38" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="20" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="31" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="69" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="35" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="85" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="120" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="119" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="20" height="1" alt="" /></td>
+		<td>
+			<img src="images/spacer.gif" width="13" height="1" alt="" /></td>
+	</tr>
+</table>
+<div class="textlinks">
+    	&nbsp;&nbsp;&nbsp;&nbsp;
+    	&laquo;
+    	<a href="index.php">HOME</a>
+    	| 
+    	<a href="news.php">NEWS</a>
+    	|
+    	<a href="features.php">FEATURES</a>
+    	|
+    	<a href="downloads.php">DOWNLOADS</a>
+    	|
+    	<a href="community.php">COMMUNITY</a>
+    	|
+    	<a href="howtohelp.php">HOW TO HELP</a>
+    	&raquo;
+</div>
+<div class="sflogo">   
+	<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=149999&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>
+</div>
+
+</body>
+</html>
