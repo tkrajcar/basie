8ebeb9d3b7b957be40777ec6ac957ff9cc34d4f5
allegroconmolto <allegroconmolto@0bc681b0-650d-0410-bb77-fdbd1d7d6fd8>
2006-02-19 06:08:56 +0000

    A couple starter classes that serve to define our source tree/package interface

---
+++ trunk/basie/src/net/sourceforge/basie/SoundFile.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.sourceforge.basie;
+
+import java.io.File;
+import java.net.URI;
+
+/**
+ * @author tkrajcar
+ */
+public class SoundFile extends File {
+	
+	
+
+	/**
+	 * @param arg0
+	 */
+	public SoundFile(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public SoundFile(URI arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public SoundFile(File arg0, String arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public SoundFile(String arg0, String arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ trunk/basie/src/net/sourceforge/basie/devices/GenericDevice.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package net.sourceforge.basie.devices;
+
+/**
+ * @author tkrajcar
+ *
+ */
+public class GenericDevice {
+	
+
+}
